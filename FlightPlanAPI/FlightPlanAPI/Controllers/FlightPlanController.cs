75a375da050fa020292658a7fcea2ee4a36df277
AritroB <77176367+AritroB@users.noreply.github.com>
2023-05-17 12:06:17 -0400

    set up environment

---
+++ FlightPlanAPI/FlightPlanAPI/Controllers/FlightPlanController.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlightPlanAPI.Controllers
+{
+    [Route("api/v1/flightplan")]
+    [ApiController]
+    public class FlightPlanController : ControllerBase
+    {
+    }
+}
